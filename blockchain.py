22d227ace086c9884a2ba403e0415f5d7c4a3dc6
Severo <severo.fabricio@gmail.com>
2018-10-27 22:10:54 -0300

    Using pickle to save and load

--- blockchain.py
+++ blockchain.py
@@ -1,6 +1,7 @@
 from functools import reduce
 from collections import OrderedDict
 import json
+import pickle
 
 from hash_util import hash_block, hash_string_256
 
@@ -20,36 +21,22 @@ participants = {owner}
 
 
 def save_data():
-    with open('blockchain.txt', mode='w') as f:
-        f.write(json.dumps(blockchain))
-        f.write('\n')
-        f.write(json.dumps(open_transactions))
+    with open('blockchain', mode='wb') as f:
+        save_data = {
+            'chain': blockchain,
+            'ot': open_transactions
+        }
+        f.write(pickle.dumps(save_data))
 
 
 def load_data():
-    with open('blockchain.txt', mode='r') as f:
-        file_content = f.readlines()
+    with open('blockchain', mode='rb') as f:
+        file_content = pickle.loads(f.read())
         global blockchain
         global open_transactions
 
-        blockchain = json.loads(file_content[0])
-        updated_blockchain = []
-        for block in blockchain:
-            updated_block = {
-                'previous_hash': block['previous_hash'],
-                'index': block['index'],
-                'proof': block['proof'],
-                'transactions': [OrderedDict([('sender', tx['sender']), ('recipient', tx['recipient']), ('amount', tx['amount'])]) for tx in block['transactions']]
-            }
-            updated_blockchain.append(updated_block)
-        blockchain = updated_blockchain
-
-        open_transactions = json.loads(file_content[1])
-        updated_transactions = []
-        for tx in open_transactions:
-            updated_transaction = OrderedDict([('sender', tx['sender']), ('recipient', tx['recipient']), ('amount', tx['amount'])])
-            updated_transactions.append(updated_transaction)
-        open_transactions = updated_transactions
+        blockchain = file_content['chain']
+        open_transactions = file_content['ot']
 
 try:
     load_data()
