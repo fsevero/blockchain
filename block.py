8c7d83b404b4a3e3d8787ae26b9bf998426610ef
Severo <severo.fabricio@gmail.com>
2018-11-19 22:26:40 -0200

    Refactor to transaction class

--- block.py
+++ block.py
@@ -1,9 +1,9 @@
 from time import time
 
 class Block:
-    def __init__(self, index, previous_hash, transactions, proof, time_=None):
+    def __init__(self, index, previous_hash, transactions, proof, timestamp=None):
         self.index = index
         self.previous_hash = previous_hash
-        self.timestamp = time() if time_ is None else time_
+        self.timestamp = time() if timestamp is None else timestamp
         self.transactions = transactions
         self.proof = proof
